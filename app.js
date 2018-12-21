7112cd0636074f0810527ec244519362f2c185ec
Danny van Lierop <github@dannyvanlierop.nl>
2018-12-21 13:13:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+myJSON = JSON.parse(fs.readFileSync("./info.json"));
+
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var routes = require("./routes/routes.js");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+routes(app);
+
+var server = app.listen(4000, function () {
+    console.log("app running on port.", server.address().port);
+})
